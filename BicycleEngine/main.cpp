4d3f0d5d86b7a95e8d344e8458bcb9ed89ce818e
AlexMassenzio <alexmassenzio@gmail.com>
2018-05-18 13:21:08 -0400

    tutorial part 5 done.

--- BicycleEngine/main.cpp
+++ BicycleEngine/main.cpp
@@ -1,22 +1,53 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(512, 512), "BicycleEngine", sf::Style::Close | sf::Style::Resize);
+	sf::RectangleShape player(sf::Vector2f(100.0f,100.0f));
+	player.setFillColor(sf::Color::Red);
 
 	while (window.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::Event evnt;
+
+		while (window.pollEvent(evnt))
 		{
-			if (event.type == sf::Event::Closed)
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
 				window.close();
+				break;
+			case sf::Event::Resized:
+				std::cout << "New window width: " << evnt.size.width << " New window height: " << evnt.size.height << std::endl;
+				break;
+			case sf::Event::TextEntered:
+				if (evnt.text.unicode < 128)
+				{
+					printf("%c", evnt.text.unicode);
+				}
+			}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+		{
+			player.move(-0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+		{
+			player.move(0.1f, 0.0f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+		{
+			player.move(0.0f, -0.1f);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+		{
+			player.move(0.0f, 0.1f);
 		}
 
 		window.clear();
-		window.draw(shape);
+		window.draw(player);
 		window.display();
 	}
 
